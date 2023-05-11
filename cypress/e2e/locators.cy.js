adcabfeedbcddb48b332ccd75b842c2505c7ee2e
metkaergul <metkaergul@gmail.com>
2023-05-10 19:01:43 -0700

    locator test updated

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -12,7 +12,15 @@ describe('find or get elements by using different locators',()=>{
         
         cy.get("input[name='username']").type('hayyy yammy kolrid'); 
         cy.get("[type='text']").clear();
+        cy.get("input").each((item ,index,list)=>{
+
+            //assert the length  of the list is 2
+            expect(list).to.have.length(2);
+                expect(item).to.have.attr('type');
+                
     })
 
+
+    })
 
 })
