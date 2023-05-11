65f379e390566fc42054b11b316e63a703ed1afa
metkaergul <metkaergul@gmail.com>
2023-05-10 18:01:27 -0700

    add: more test

--- cypress/e2e/testStructure.cy.js
+++ cypress/e2e/testStructure.cy.js
@@ -17,5 +17,23 @@ describe('Context: My First Tests', () => {
     it('Opening a web application', () => {
         cy.visit('/registration_form');
     })
+    it('Test2',()=>{
+
+        expect(false).to.equal(false);
+      
+    })
+    it('Test3',()=>{
+
+        expect(false).not.to.equal(true);
+    })
+    it('Test4',()=>{
+
+        expect(5).to.equal(5);
+    })
+    it('Test5',()=>{
+
+        expect(true).to.equal(5=='5');
+    })
+
 
 })
