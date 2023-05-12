3dff403b8404e35f646e182c696e1cbf0bc5e616
metkaergul <metkaergul@gmail.com>
2023-05-12 14:39:41 -0700

    locator strategies added

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -11,7 +11,11 @@ describe('find or get elements by using different locators',()=>{
     it('check different locator strategies',()=>{
         
         cy.get("input[name='username']").type('hayyy yammy kolrid'); 
+
+        //attribute name and value
         cy.get("[type='text']").clear();
+
+        //tagName
         cy.get("input").each((item ,index,list)=>{
 
             //assert the length  of the list is 2
@@ -19,6 +23,18 @@ describe('find or get elements by using different locators',()=>{
                 expect(item).to.have.attr('type');
                 
     })
+
+    //by attribute name
+    cy.get('[type]')
+
+    //by class attribute's value
+    cy.get('.btn.btn-primary')
+
+    //by id
+    cy.get('#wooden_spoon')
+
+    //if we want to use text of the web element how we do this with cypress 
+    cy.get('button').should('contain','Login').click()
 
 
     })
