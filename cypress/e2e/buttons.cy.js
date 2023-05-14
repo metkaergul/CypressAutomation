d98e051b219667d8cd32e4713a362f1c6a7015ac
metkaergul <metkaergul@gmail.com>
2023-05-13 21:04:26 -0700

    assertions added

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -21,9 +21,36 @@ describe('find or get elements by using different locators',()=>{
             cy.wrap($buttons).eq(2).click();
             //assert the textx
             cy.contains('Clicked on button three!').should('be.visible')
-
-        
         })
+
+
+        cy.get('button').each((item,index,list)=>{
+
+            //assert the length of the list which means verifying number of buttons
+            
+                expect(list).to.have.length(6)
+                expect(item).to.have.attr('onclick')
+
+
+        })
+
+        //i will get all buttons like previous approach ,get only the item then check for each text of the item if
+        //if it is equal to 'Button 4', then click on it
+        
+        
+        cy.get('button').each((item)=>{
+       
+                if((item.text()== 'Button 4')){
+                
+                    cy.log(item.text())
+                    //item.click()    you can not use cypress click with jQuery element 'item'
+                    cy.wrap(item).click();
+                    cy.contains('Clicked on button four!').should('be.visible')
+                }
+
+
+        })
+
 
     })
 
